2421baf03213c2778b694c9e693d10bc81235a04
Anand B <anand-b@users.noreply.github.com>
2019-03-24 23:44:40 -0500

    Create basic project structure and readme.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lazy To Cook
+App that lets people order food from restaurants and get it delivered at their doorstep.
+
+#### Technology Stack
+1. Node.js
+2. MySQL
+3. Express.js
+4. React.js
+
+##### Steps To Setup Project:
+1.  Install [Node](https://nodejs.org/en/), NPM and [MySQL](https://dev.mysql.com/downloads/) in your machine.
+2.  Clone this repository.
+3.  Open terminal and change current working directory to <root>/app
+4.  Start the MySQL server.
+5.  Edit the `db_init_queries.json` file with any queries you want to add. 
+6.  Run `npm run setup` (Use `sudo`, if required)
+
+Now, all the required backend dependencies will the installed and the database will be created.
+
+**Note:** Please use this command only for first installation. Do not use it on production database as it drops existing tables from database and re-creates them.
---
+++ app/index.js
@@ -0,0 +1,62 @@
+var mysql = require("mysql");
+var queries = require("./db_init_queries.json");
+
+var host = 'localhost';
+var port = 8889;
+var username = 'root';
+var password = 'root';
+var db_name = 'lazytocook';
+
+var connection = mysql.createConnection({
+  host      : host,
+  port		: port,
+  user      : username,
+  password  : password
+});
+
+var runQueries = function(connection, index) {
+	if (index >= queries.length) {
+		console.log("Closing connection.");
+		connection.end();
+		return;
+	}
+	console.log("Executing: "+queries[index]);
+	connection.query(queries[index], function (err, results) {
+	  if (err) throw err;
+	  console.log("Done.");
+	  runQueries(connection, index+1);
+	});
+} 
+
+var createDatabase = function() {
+	connection.query("CREATE DATABASE "+db_name+";", function (err, result) {
+		if (err) {
+			throw err;
+		}
+		console.log("Database created.");
+		connection.query("USE "+db_name+";");
+		runQueries(connection, 0);
+		// connection.end();
+	});
+}
+
+var dropExistingDatabase = function() {
+	connection.query("DROP DATABASE IF EXISTS "+db_name+";", function(err, result) {
+		if (err) {
+			throw err;
+		}
+		createDatabase();
+	});
+}
+
+connection.connect(function(err) {
+  if (err) {
+  	throw err;
+  }
+  console.log("Connected to MySQL!");
+  dropExistingDatabase();
+});
+
+
+ 
+// connection.end();
