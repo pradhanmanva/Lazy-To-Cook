3bdc7e9b6d52e412c117a8a932a5a312d46a43d9
Manva Pradhan <pradhanmanva@gmail.com>
2019-03-30 16:28:45 -0500

    setup

--- app/index.js
+++ app/index.js
@@ -9,55 +9,54 @@ var password = 'root';
 var db_name = 'lazytocook';
 
 var connection = mysql.createConnection({
-  host      : host,
-  port		: port,
-  user      : username,
-  password  : password
+    host: host,
+    port: port,
+    user: username,
+    password: password
 });
 
-var runQueries = function(connection, index) {
-	if (index >= queries.length) {
-		console.log("Closing connection.");
-		connection.end();
-		return;
-	}
-	console.log("Executing: "+queries[index].bold);
-	connection.query(queries[index], function (err, results) {
-	  if (err) throw err;
-	  console.log("Done.".green);
-	  runQueries(connection, index+1);
-	});
-} 
+var runQueries = function (connection, index) {
+    if (index >= queries.length) {
+        console.log("Closing connection.");
+        connection.end();
+        return;
+    }
+    console.log("Executing: " + queries[index].bold);
+    connection.query(queries[index], function (err, results) {
+        if (err) throw err;
+        console.log("Done.".green);
+        runQueries(connection, index + 1);
+    });
+};
 
-var createDatabase = function() {
-	connection.query("CREATE DATABASE "+db_name+";", function (err, result) {
-		if (err) {
-			throw err;
-		}
-		console.log("Database created.".green);
-		connection.query("USE "+db_name+";");
-		runQueries(connection, 0);
-		// connection.end();
-	});
-}
+var createDatabase = function () {
+    connection.query("CREATE DATABASE " + db_name + ";", function (err, result) {
+        if (err) {
+            throw err;
+        }
+        console.log("Database created.".green);
+        connection.query("USE " + db_name + ";");
+        runQueries(connection, 0);
+        // connection.end();
+    });
+};
 
-var dropExistingDatabase = function() {
-	connection.query("DROP DATABASE IF EXISTS "+db_name+";", function(err, result) {
-		if (err) {
-			throw err;
-		}
-		createDatabase();
-	});
-}
+var dropExistingDatabase = function () {
+    connection.query("DROP DATABASE IF EXISTS " + db_name + ";", function (err, result) {
+        if (err) {
+            throw err;
+        }
+        createDatabase();
+    });
+};
 
-connection.connect(function(err) {
-  if (err) {
-  	throw err;
-  }
-  console.log("Connected to MySQL!".green);
-  dropExistingDatabase();
+connection.connect(function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("Connected to MySQL!".green);
+    dropExistingDatabase();
 });
 
 
- 
 // connection.end();
