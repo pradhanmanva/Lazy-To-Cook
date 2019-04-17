80ac3479d506602759eff8d888a58a8861cc6ac1
Anand B <anand-b@users.noreply.github.com>
2019-04-17 13:48:38 -0500

    Refactor authentication

---
+++ app/src/router/AuthRouter.js
@@ -0,0 +1,103 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+
+const RestaurantHandler = require("../handlers/RestaurantHandler");
+const RestaurantAuthenticationModel = require("../models/RestaurantAuthenticationModel");
+
+const UserHandler = require("../handlers/UserHandler");
+const UserAuthenticationModel = require("../models/UserAuthenticationModel");
+const UserModel = require("../models/UserModel");
+
+class AuthRouter {
+    constructor(app) {
+        this.app = app;
+        this._validateCredentials = this._validateCredentials.bind(this);
+    }
+
+    _login(request, response, next) {
+        /**
+        * @requires request.body {
+        *   password : "",
+        *   username : "",
+        *   user_type : "admin"[ or "user"]
+        * }
+        */
+        passport.authenticate('local', (err, user, info) => {
+            if (user) {
+                request.login(user, (err) => {
+                    return response.status(200).send('Success!').end();
+                })
+            } else {
+                return response.status(500).send('Invalid credentials!').end();
+            }
+        })(request, response, next);
+    }
+
+    _logout(request, response, next) {
+        request.logout();
+        response.status(200).send("Successfully logged out!").end();
+    }
+
+    _userSerializer(user, done) {
+        done(null, `${user.user_id}::${user.type}`);
+    }
+
+    _userDeserializer(id, done) {
+        return done(null, {
+            "user_id" : id.split("::")[0],
+            "type" : id.split("::")[1]
+        });
+    }
+
+    _validateRestaurant(username, password, done) {
+        const authCredentials = new RestaurantAuthenticationModel(null, username, password, false);
+        new RestaurantHandler().validate(authCredentials).then(function(restaurantId) {
+            return done(null, {"user_id" : restaurantId, type : "admin"});
+        }).catch(function(error) {
+            return done(null, false, { message : error});
+        });
+    }
+
+    _validateUser(username, password, done) {
+        const userModel = new UserModel(null, null, null, null, null, username, null);
+        const authCredentials = new UserAuthenticationModel(userModel, password, false);
+        new UserHandler().validate(authCredentials).then(function(userId) {
+            return done(null, {"user_id" : userId, type : "user"});
+        }).catch(function(error) {
+            return done(null, false, { message : error});
+        });
+    }
+
+    _validateCredentials(request, username, password, done) {
+        if (request.body.user_type === "admin") {
+            this._validateRestaurant(username, password, done);
+        }
+        else if (request.body.user_type === "user") {
+            this._validateUser(username, password, done);
+        }
+        else {
+            return done(null, false, {message : 'Unrecognized user type.'});
+        }
+    }
+
+    wire() {
+        const self = this;
+        const localStrategy = new LocalStrategy(
+            { 
+                usernameField: 'username', 
+                passwordField : 'password', 
+                passReqToCallback : true 
+            },
+            self._validateCredentials
+        );
+        passport.use(localStrategy);
+        passport.serializeUser(self._userSerializer);
+        passport.deserializeUser(self._userDeserializer);
+        this.app.use(passport.initialize());
+        this.app.use(passport.session());
+        this.app.post("/auth/login", self._login);
+        this.app.get("/auth/logout", self._logout);
+    }
+}
+
+module.exports = AuthRouter;
---
+++ app/src/router/RegistrationRouter.js
@@ -0,0 +1,96 @@
+const RestaurantHandler = require("../handlers/RestaurantHandler");
+const RestaurantAuthenticationModel = require("../models/RestaurantAuthenticationModel");
+const RestaurantModel = require("../models/RestaurantModel");
+
+const UserHandler = require("../handlers/UserHandler");
+const UserAuthenticationModel = require("../models/UserAuthenticationModel");
+const UserModel = require("../models/UserModel");
+const AddressModel = require("../models/AddressModel");
+
+
+class RegistrationRouter  {
+    constructor(app) {
+        this.app = app;
+    }
+
+    _registerRestaurant(data, response) {
+        /**
+        * @requires request.body {
+        *   name : "",
+        *   contact : "",
+        *   email : "",
+        *   website : "",
+        *   password : "",
+        *   username : "",
+        *   user_type : "admin"
+        * }
+        */
+        const restaurantModel = new RestaurantModel(null, data.name, data.contact, data.email, data.website);
+        const authCredentials = new RestaurantAuthenticationModel(restaurantModel, data.username, data.password, true);
+
+        new RestaurantHandler().register(authCredentials).then(function(insertedRestaraunt) {
+            if (insertedRestaraunt) {
+                insertedRestaraunt = insertedRestaraunt.toJSON();
+            } else {
+                insertedRestaraunt = {};
+            }
+            response.status(200).json(insertedRestaraunt).end();
+        }).catch(function(error) {
+            console.error(error);
+            response.status(500).send("Error occurred while creating a restaurant. Please check logs for details.").end();
+        });
+    }
+
+    _registerUser(data, response) {
+        /**
+        * @requires request.body {
+        *   first_name : "",
+        *   middle_name : "",
+        *   last_name : "",
+        *   dob : "",
+        *   email : "",
+        *   password : "",
+        *   user_type : "user",
+        *   address : {
+        *       line1 : "",
+        *       line2 : "",
+        *       city : "",
+        *       state : "",
+        *       zipcode : numeric
+        *   }
+        * }
+        */
+        const addressModel = new AddressModel(null, data.address.line1, data.address.line2, data.address.city, data.address.state, data.address.zipcode);
+        const userModel = new UserModel(null, data.first_name, data.middle_name, data.last_name, data.dob, data.email, addressModel);
+        const authCredentials = new UserAuthenticationModel(userModel, data.password, true);
+
+        new UserHandler().register(authCredentials).then(function(insertedUser) {
+            if (insertedUser) {
+                insertedUser = insertedUser.toJSON();
+            } else {
+                insertedUser = {};
+            }
+            response.status(200).json(insertedUser).end();
+        }).catch(function(error) {
+            console.error(error);
+            response.status(500).send("Error occurred while creating a user. Please check logs for details.").end();
+        });
+    }
+
+    wire() {
+        this.app.post("/auth/register", (request, response) => {
+            const data = request.body;
+            if (data.user_type === "admin") {
+                this._registerRestaurant(data, response);
+            }
+            else if (data.user_type === "user") {
+                this._registerUser(data, response);
+            }
+            else {
+                response.status(500).send("Unrecognized user type.").end();
+            }
+        });
+    }
+}
+
+module.exports = RegistrationRouter;
