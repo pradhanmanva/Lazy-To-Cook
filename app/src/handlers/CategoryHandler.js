b010d771049035ce3952dc76d80c376baec4d5ed
Anand B <anand-b@users.noreply.github.com>
2019-04-21 02:22:59 -0500

    Add check to not list soft deleted items for users.

--- app/src/handlers/CategoryHandler.js
+++ app/src/handlers/CategoryHandler.js
@@ -10,7 +10,7 @@ class CategoryHandler {
 
     fetchAll() {
         const dbUtil = new DBUtil();
-        const selectQuery = `SELECT * FROM ${CATEGORY_TABLE.NAME} INNER JOIN ${RESTAURANT_TABLE.NAME} ON ${CATEGORY_TABLE.NAME}.${CATEGORY_TABLE.COLUMNS.RESTAURANT}=${RESTAURANT_TABLE.NAME}.${RESTAURANT_TABLE.COLUMNS.ID}`;
+        const selectQuery = `SELECT * FROM ${CATEGORY_TABLE.NAME} INNER JOIN ${RESTAURANT_TABLE.NAME} ON ${CATEGORY_TABLE.NAME}.${CATEGORY_TABLE.COLUMNS.RESTAURANT}=${RESTAURANT_TABLE.NAME}.${RESTAURANT_TABLE.COLUMNS.ID} WHERE ${CATEGORY_TABLE.NAME}.${CATEGORY_TABLE.COLUMNS.IS_DELETED} = false AND ${RESTAURANT_TABLE.NAME}.${RESTAURANT_TABLE.COLUMNS.IS_DELETED} = false`;
         return dbUtil.getConnection().then(function(connection) {
             if (!connection) {
                 throw Error('connection not available.');
