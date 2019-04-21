cb45a65266b9e61f070a4204a9341283b414e829
Manva Pradhan <pradhanmanva@gmail.com>
2019-04-21 12:15:21 -0500

    Updated CartItemRouter and CartItemHandler

--- app/src/handlers/CartItemHandler.js
+++ app/src/handlers/CartItemHandler.js
@@ -1,10 +1,12 @@
 const CartModel = require("../models/CartModel");
 const CartItemModel = require("../models/CartItemModel");
+const ItemModel = require("../models/ItemModel");
 
 const DBUtil = require("../utils/DBUtil");
 
 const CART_TABLE = require("../tables/CartTable");
 const CARTITEM_TABLE = require("../tables/CartItemTable");
+const ITEM_TABLE = require("../tables/ItemTable");
 
 class CartItemHandler {
     constructor() {
@@ -14,7 +16,7 @@ class CartItemHandler {
     fetchAll(cart) {
         if (cart && cart.id) {
             const dbUtil = new DBUtil();
-            const selectQuery = `SELECT * FROM ${CARTITEM_TABLE.NAME} WHERE ${CARTITEM_TABLE.COLUMNS.ID} = ?`;
+            const selectQuery = `SELECT * FROM ${CARTITEM_TABLE.NAME} JOIN ${ITEM_TABLE.NAME} ON ${CARTITEM_TABLE.COLUMNS.ITEM}=${ITEM_TABLE.COLUMNS.ID} WHERE ${CARTITEM_TABLE.NAME}.${CARTITEM_TABLE.COLUMNS.ID} = ?`;
             return dbUtil.getConnection().then(function (connection) {
                 if (!connection) {
                     throw Error('connection not available.');
@@ -24,7 +26,7 @@ class CartItemHandler {
                 return result.results.map(function (result, index, arr) {
                     return new CartItemModel(
                         String(result[CARTITEM_TABLE.COLUMNS.ID]),
-                        new ItemModel(),
+                        new ItemModel(result[ITEM_TABLE.COLUMNS.ID], result[ITEM_TABLE.COLUMNS.NAME], result[ITEM_TABLE.COLUMNS.DESCRIPTION], result[ITEM_TABLE.COLUMNS.PRICE], result[ITEM_TABLE.COLUMNS.CATEGORY]),
                         result[CARTITEM_TABLE.COLUMNS.QUANTITY]
                     );
                 });
